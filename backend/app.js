1c38adfcfcc8b0c9894447197be92d23f818aefe
Jay Peek <73311799+ChefJayPeek@users.noreply.github.com>
2021-01-28 17:42:59 -0600

    Fixing a few issues for Heroku deploy

--- backend/app.js
+++ backend/app.js
@@ -14,25 +14,31 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+app.use(express.static(path.join('public')));
 
+// CORS headers for development
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//         );
+//         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+        
+//         next();
+//     });
+    
+    app.use('/api/dishes', dishesRoutes);
+    app.use('/api/users', usersRoutes);
+    
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-        );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-
-    next();
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
-app.use('/api/dishes', dishesRoutes);
-app.use('/api/users', usersRoutes);
-
-app.use((req, res, next) => {
-    const error = new HttpError('Could not find this route.', 404);
-    throw error;
-});
+// app.use((req, res, next) => {
+//     const error = new HttpError('Could not find this route.', 404);
+//     throw error;
+// });
 
 app.use((error, req, res, next) => {
     if (req.file) {
