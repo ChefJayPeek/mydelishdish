9b74177b63f981e56222f0664c814b51f0ef4a11
Jay Peek <73311799+ChefJayPeek@users.noreply.github.com>
2021-01-29 09:14:22 -0600

    Working on heroku, readme.md updated

---
+++ backend/public/precache-manifest.6dcac34e1dbd3bfbb47e6e7e9fe3a90c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab24d04eb1f526199239e047af04807d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98a398aeaf61ab9f1fd7",
+    "url": "/static/css/main.35cc7b8e.chunk.css"
+  },
+  {
+    "revision": "618bab09f8b931c9ed91",
+    "url": "/static/js/2.8f69e2c0.chunk.js"
+  },
+  {
+    "revision": "98a398aeaf61ab9f1fd7",
+    "url": "/static/js/main.7834f47c.chunk.js"
+  },
+  {
+    "revision": "51810371bf2cf35f1e22",
+    "url": "/static/js/runtime-main.85498786.js"
+  }
+]);
